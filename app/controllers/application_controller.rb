0e445cbc2a233f129ddb353754d20f5cc4b55df6
Vinh Do <vinhdo07@gmail.com>
2019-04-04 05:24:19 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,7 @@ class ApplicationController < Sinatra::Base
 
   patch "/articles/:id" do
     @article = Article.find(params[:id])
-  
+    @article.update(params)
   end
 
   delete '/articles/:id' do
